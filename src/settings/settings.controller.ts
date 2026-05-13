885d7e5675c53789e288877bbca03083e71efb36
Dev Siraj <58474312+Khan-Siraj@users.noreply.github.com>
2026-05-14 00:01:55 +0530

    dashboard and setting crud

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { SettingsService } from './settings.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('settings')
-export class SettingsController {}
+export class SettingsController {
+  constructor(private settingsService: SettingsService) {}
+
+  @Get()
+  getSettings(@Req() req: any) {
+    return this.settingsService.getSettings(req.user);
+  }
+
+  @Patch()
+  updateSettings(@Body() body: any, @Req() req: any) {
+    return this.settingsService.updateSettings(body, req.user);
+  }
+}
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+  constructor(private prisma: PrismaService) {}
+
+  getSettings(user: any) {
+    return this.prisma.setting.upsert({
+      where: {
+        organizationId: user.organizationId,
+      },
+      update: {},
+      create: {
+        organizationId: user.organizationId,
+      },
+    });
+  }
+
+  updateSettings(dto: any, user: any) {
+    return this.prisma.setting.upsert({
+      where: {
+        organizationId: user.organizationId,
+      },
+      update: {
+        defaultLowStockValue: Number(dto.defaultLowStockValue),
+      },
+      create: {
+        organizationId: user.organizationId,
+        defaultLowStockValue: Number(dto.defaultLowStockValue),
+      },
+    });
+  }
+}
